235bf47374d0f4fcad82f86d35204cabda06aa5c
Conlan <conlanc@gmail.com>
2016-01-29 19:17:53 -0500

    Calibrate the IMU on startup

--- src/org/usfirst/frc/team2713/robot/input/imu/IMU.java
+++ src/org/usfirst/frc/team2713/robot/input/imu/IMU.java
@@ -6,14 +6,24 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class IMU {
-	ADIS16448_IMU imu;
+	ADIS16448_IMU imu = new ADIS16448_IMU();
 
 	public void initImu() {
-		imu = new ADIS16448_IMU();
-		while (true) {
+		System.out.println("Calibarating IMU, do NOT touch the robot...");
+		imu.calibrate();
+		System.out.println("Calibration Complete, feel free to move about the robot");
+		while (true) { //TODO Does this get stuck in a loop?
             SmartDashboard.putData("IMU", imu);
             Timer.delay(0.005);		// wait for a motor update time
         }
+	}
+	
+	public double getAngle(){
+		double angle = imu.getAngle();
+		if (angle > 360){
+			angle = angle - 360; // The gyro by default goes over 360 (ex. 360 -> 361 -> 362 etc.)
+		}
+		return angle;
 	}
 
 }
