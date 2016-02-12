21f80abbfcebd02f9eb44f0f010056644a2bd627
mmiillkkaa <mmiillkkaa@users.noreply.github.com>
2016-02-11 19:03:52 -0500

    Woospy, I forgot this part.

---
+++ src/org/usfirst/frc/team2713/robot/subsystems/BaseSubsystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2713.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class BaseSubsystem extends Subsystem {
+
+	protected abstract void initDefaultCommand();
+	
+	public void startTeleop() {}
+	public void startAuto(int chosen) {}
+	public void startDisabled() {}
+
+}
