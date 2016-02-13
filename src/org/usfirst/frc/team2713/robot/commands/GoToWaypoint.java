2fbc30a2420cc6aecef4e48b0916578c3c15e63e
ryanb <ryanbradford333@gmail.com>
2016-02-13 18:30:47 +0100

    Corrected some angle stuff.

--- src/org/usfirst/frc/team2713/robot/commands/GoToWaypoint.java
+++ src/org/usfirst/frc/team2713/robot/commands/GoToWaypoint.java
@@ -12,7 +12,17 @@ public class GoToWaypoint extends CommandGroup {
 		double yDistance = waypointY - lastY;
 		double xDistance = waypointX - lastX;
 		double distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-		double angle = Math.asin(yDistance / xDistance) * 180 / Math.PI;
+		double angle = drive.imu.getAngle();
+		if(distance != 0) {
+			angle = Math.asin(Math.abs(yDistance) / distance) * 180 / Math.PI;
+		}
+		if(xDistance < 0 && yDistance < 0) {
+			angle = angle - 180;
+		} else if((xDistance >= 0 && yDistance < 0)) {
+			angle = Math.asin(yDistance / distance);
+		} else if(xDistance < 0 && yDistance >= 0) {
+			angle = Math.acos(xDistance / distance);
+		}
 		this.addSequential(new GoToAngle(drive, angle));
 		this.addSequential(new GoForward(drive, distance, 1));
 	}
